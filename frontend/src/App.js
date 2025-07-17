ef4190a3aea1ed4c88b93158d4a4f80817d93e0e
Nouha <berguiganouha62@gmail.com>
2025-07-17 18:39:54 +0200

    formations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
  import Login from"./components/login";
- import Home from"./components/home"; 
+
+ import FormationTable from "./components/FormationTable"; 
 function App() {
  return (
  <Router>
  <Routes>
  <Route path="/" element={<Login />} />
- <Route path="/home" element={<Home />} />
+ 
  <Route path="/formations" element={<FormationTable />} /> 
 
  </Routes>
