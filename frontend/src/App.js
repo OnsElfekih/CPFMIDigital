d16811b128155d8050def2f6477b037541b3e9f8
sarraWeslati <weslatisara1@gmail.com>
2025-07-17 20:20:45 +0100

    planning 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
- import Login from"./components/login";
- import Home from"./components/home"; // Page après connexion
+import Login from "./components/login";
+import HomeAdmin from "./components/homeAdmin";
+import HomeFormateur from "./components/homeFormateur";
+import HomeEntreprise from "./components/homeEntreprise";
+import ViewPlanning from "./components/ViewPlanning";
+import FormMotdepasse from "./components/FormNMotDePasse";
+//import CalendarCard from "./components/CalendarCard"; // Si utilisé
+
 function App() {
- return (
- <Router>
- <Routes>
- <Route path="/" element={<Login />} />
- <Route path="/home" element={<Home />} />
- </Routes>
- </Router>
- );
- }
- export default App
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<HomeAdmin />} />
+        <Route path="/formateur" element={<HomeFormateur />} />
+        <Route path="/entreprise" element={<HomeEntreprise />} />
+        <Route path="/planning" element={<ViewPlanning />} />
+        <Route path="/mdp" element={<FormMotdepasse />} />
+        {/* Ajoute d'autres routes ici si nécessaire */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
