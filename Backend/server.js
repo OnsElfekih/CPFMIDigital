8719d6b9294fd87d96886018e92fc1e9d4906e48
Ons Elfekih <elfekihons@gmail.com>
2025-07-17 22:32:57 +0100

    correctionpathetserver

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const cors = require("cors");
-const users = require ("./routes/api/users");
-const formations = require("./routes/api/formations"); 
 
 const users = require("./routes/api/users");
+const formations = require("./routes/api/formations");
 const formateurRoutes = require("./routes/api/formateurRoutes");
 const planningRoutes = require("./routes/api/planningRoutes");
 
@@ -15,27 +14,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-mongoose.set('strictQuery', true);
-mongoose
-.connect(mongo_url)
-.then(() => console.log("MongoDB connected..."))
-.catch((err) => console.log(err));
-app.use ("/users",users);
-app.use("/api/formations", formations);
-// Routes
-//app.use("/users", users);
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/planning", planningRoutes);
-app.use("/api/formateurs", formateurRoutes);
-
 // Connexion MongoDB
-const mongo_url = config.get("mongo_url");
+const mongo_url = "mongodb://localhost:27017/CPFMI"; // ta base CPFMI
+
 mongoose.set("strictQuery", true);
 mongoose
   .connect(mongo_url)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
+
+// Routes
+app.use("/users", users);
+app.use("/api/formations", formations);
+app.use("/api/planning", planningRoutes);
+app.use("/api/formateurs", formateurRoutes);
 
 // Démarrage du serveur
 const port = process.env.PORT || 3001;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
  import Login from"./components/login";
- import Home from"./components/home"; // Page après connexion
+import HomeAdmin from "./components/homeAdmin";
+import HomeEntreprise from "./components/homeEntreprise";
+import HomeFormateur from "./components/homeFormateur";
+import FormNMotDePasse from "./components/formnmotdepasse";
+import CalendarCard from "./components/CalendarCard";
+import FormationForm from "./components/FormationForm";
+import ViewPlanning from "./components/ViewPlanning";
+import FormationTable from "./components/FormationTable";
 function App() {
  return (
  <Router>
  <Routes>
  <Route path="/" element={<Login />} />
- <Route path="/home" element={<Home />} />
+  <Route path="/homeadmin" element={<HomeAdmin/>} />
+   <Route path="/homeentreprise" element={<HomeEntreprise/>} />
+    <Route path="/homeformateur" element={<HomeFormateur />} />
+    <Route path="/formnmotdepasse" element={<FormNMotDePasse/>} />
+    <Route path="/CalendarCard" element={<CalendarCard/>} />
+    <Route path="/FormationForm" element={<FormationForm/>} />
+     <Route path="/Planning" element={<ViewPlanning/>} />
+    <Route path="/formations" element={<FormationTable/>} />
  </Routes>
  </Router>
  );
